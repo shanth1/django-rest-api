249511530867f87b7ac3337bd4ce72dd91100dfe
Denis Beresnev <shanthi.bunch@gmail.com>
2024-01-03 00:07:12 +0300

    feat: Connected chat to core
    #python manage.py createapp chat
    python manage.py makemigrations

--- backend/chat/models.py
+++ backend/chat/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Chat(models.Model):
+    message = models.TextField(blank=True)
+    time_create = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.message
